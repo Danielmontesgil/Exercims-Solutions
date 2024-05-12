7016453f1e352c3430c79da3b4dee1be0bf572ca
Daniel Montes Gil <dmontesgil@gmail.com>
2024-05-11 21:43:42 -0500

    Lucian's Luscious Lasagna Exercise

---
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,7 @@
+class Lasagna
+{
+    public int ExpectedMinutesInOven() => 40;
+    public int RemainingMinutesInOven(int actualMinutes) => ExpectedMinutesInOven() - actualMinutes;
+    public int PreparationTimeInMinutes(int numberOfLayers) => 2 * numberOfLayers;
+    public int ElapsedTimeInMinutes(int numberOfLayers, int b) => PreparationTimeInMinutes(numberOfLayers) + b;
+}
